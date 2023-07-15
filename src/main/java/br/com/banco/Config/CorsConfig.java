53bbaf54c9bee058eebd7cbb661736f361faccde
Grezewave <Vitor.P.555@gmail.com>
2023-07-15 15:04:39 -0300

    Cors config

---
+++ src/main/java/br/com/banco/Config/CorsConfig.java
@@ -0,0 +1,22 @@
+package br.com.banco.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*"); //Pra funcionar no React
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+}
